52a7ba19a8d99af036e87f230ff180387b0676a5
Riccardo Sacco <rickybag99@gmail.com>
2020-10-20 15:43:40 +0200

    Updates

---
+++ precache-manifest.c1c5f1220b622082d09f644cef7397de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03a1215144108816c9befaf6654ed027",
+    "url": "/matching-engine-frontend/index.html"
+  },
+  {
+    "revision": "56f2cc063214206af637",
+    "url": "/matching-engine-frontend/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a83f1f2ce1dc5f949305",
+    "url": "/matching-engine-frontend/static/js/2.af6dc0f9.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/matching-engine-frontend/static/js/2.af6dc0f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56f2cc063214206af637",
+    "url": "/matching-engine-frontend/static/js/main.ef23bf76.chunk.js"
+  },
+  {
+    "revision": "653fcd317dd57894d612",
+    "url": "/matching-engine-frontend/static/js/runtime-main.a9fddad8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/matching-engine-frontend/static/media/logo.5d5d9eef.svg"
+  }
+]);
