d3073157be2e1411417d92622416d6901d88822d
rickybag <rickybag99@gmail.com>
2020-10-28 11:47:02 +0100

    updated urls

--- src/globals.ts
+++ src/globals.ts
@@ -1,4 +1,4 @@
 export default {
-  API_URL: "https://fqma0mxypb.execute-api.eu-central-1.amazonaws.com/v1",
-  ELASTIC_URL: "https://search-matching-engine-nhygzot7nqafdvrzb3niwryuay.eu-central-1.es.amazonaws.com"
+  API_URL: "https://2268a03vsh.execute-api.eu-west-1.amazonaws.com/v1",
+  ELASTIC_URL: "https://search-matching-engine-uv2ckzsmrpytmltjko7x5jf4ra.eu-west-1.es.amazonaws.com"
 }
